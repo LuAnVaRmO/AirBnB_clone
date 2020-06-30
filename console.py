c5e0a9d989945f054d22dd6e3b981ac07262fb01
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-06-30 15:48:41 -0500

    task 7

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ from models.base_model import BaseModel
 import shlex
 classes = ["BaseModel", "User", "State", "City", "Amenity", "Place", "Review"]
 
+
 class HBNBCommand(cmd.Cmd):
     """ Class command """
     prompt = '(hbnb)'
@@ -25,7 +26,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, arg):
-        """Create a new class instance, usage: (hbnb) create <name of class>"""
+        """Create a new class instance, usage: (hbnb) create <class.name>"""
         argv = shlex.split(arg)
         if len(argv) == 0:
             print("** class name missing **")
@@ -37,9 +38,9 @@ class HBNBCommand(cmd.Cmd):
             return
         print(instance.id)
         instance.save()
-    
+
     def do_show(self, arg):
-        """ Print an instance like a string, usage (hbnb) show <class.name> <class.id>"""
+        """ Print an instance like a string, usage: (hbnb) show <name> <id> """
         argv = shlex.split(arg)
         if len(argv) == 0:
             print("** class name missing **")
@@ -50,10 +51,82 @@ class HBNBCommand(cmd.Cmd):
         if argv[0] not in classes:
             print("** class doesn't exist **")
             return
-        if argv[0] in classes:
-            k = argv[0] + "." + argv[1] # usage
+        else:
+            k = argv[0] + "." + argv[1]
             if k in models.storage.all():
                 print(models.storage.all()[k])
+            else:
+                print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """Destroy an instance, usage: destroy <class.name> <class.id> """
+        argv = shlex.split(arg)
+        if len(argv) == 0:
+            print("** class name missing **")
+            return
+        if len(argv) == 1:
+            print("** instance id missing **")
+            return
+        if argv[0] not in classes:
+            print("** class doesn't exist **")
+            return
+        else:
+            k = argv[0] + "." + argv[1]  # usage
+            if k in models.storage.all():
+                models.storage.all().pop(k)
+                models.storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        """Prints string rep of all instances based or not on the class name"""
+        argv = shlex.split(arg)
+        if len(argv) == 0:
+            for item in models.storage.all():
+                print(item)
+        elif argv[0] in classes:
+            for mod in models.storage.all():
+                print(models.storage.all()[mod])
+        else:
+            print("** class doesn't exist **")
+
+    def do_update(self, arg):
+        """Update or add a new attribute from an instanced class. """
+        argv = shlex.split(arg)
+        if len(argv) == 0 or argv[0] not in classes:
+            print("** class name missing **")
+            return
+        if len(argv) == 1:
+            print("** instance id missing **")
+            return
+        if len(argv) > 1:
+            k = argv[0] + "." + argv[1]
+            if k not in models.storage.all():
+                print("** no instance found **")
+                return
+            elif len(argv) == 2:
+                print("** attribute name missing **")
+                return
+        if len(argv) == 3:
+            print("** value missing **")
+            return
+        if argv[0] not in classes:
+            print("** class doesn't exist **")
+            return
+        else:
+            k = argv[0] + "." + argv[1]
+            if k in models.storage.all():
+                if argv[2] == 'id':
+                    return
+                if argv[2] == 'created_at' or argv[2] == 'updated_at':
+                    return
+                if isinstance(argv[2], int) is True:
+                    argv[3] = int(args[3])
+                elif isinstance(argv[2], float):
+                    argv[3] = float(argv[3])
+                setattr(models.storage.all()[k], argv[2], argv[3])
+                models.storage.all()[k].save()
+                return
             else:
                 print("** no instance found **")
 
