e4e26460d1c12d50833f10cc15b59e3c8e99313f
nickarshadi <1477@holbertonschool.com>
2020-06-30 17:56:41 -0500

    Create first test files

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 import unittest
+import models
+from models.base_model import BaseModel
 
 
-class Test_BaseModel(unittest.testCase):
+class Test_BaseModel(unittest.TestCase):
     """ Test class BaseModel """
-    
+    def setUp(self):
+        self.model = BaseModel()
+
+    def test_instantiation(self):
+        """ """
+        self.assertIsInstance(self.model, BaseModel)
+
+    def test_atrributes(self):
+        """ """
+        self.assertTrue(hasattr(self.model, "created_at"))
+        self.assertTrue(hasattr(self.model, "id"))
+        self.assertTrue(hasattr(self.model, "updated_at"))
